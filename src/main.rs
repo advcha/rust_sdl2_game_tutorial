16d2130f8bc3c276774f7dd2e99ba59c3c8ccf2a
advcha <advcha@yahoo.com>
2021-12-16 21:58:04 +0700

    fixed some errors. removed 'world.res'. update sdl and spec version

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,25 @@
-use sdl2::pixels::Color;
+mod components;
+mod physics;
+mod animator;
+mod keyboard;
+mod renderer;
+
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
-use sdl2::render::{WindowCanvas, Texture};
+use sdl2::pixels::Color;
 use sdl2::rect::{Point, Rect};
 // "self" imports the "image" module itself as well as everything else we listed
 use sdl2::image::{self, LoadTexture, InitFlag};
+
+use specs::prelude::*;
+
 use std::time::Duration;
 
-const PLAYER_MOVEMENT_SPEED:i32 = 20;
+use crate::components::*;
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-enum Direction {
-    Up,
-    Down,
-    Left,
-    Right,
-}
-
-#[derive(Debug)]
-struct Player {
-    position: Point,
-    sprite: Rect,
-    speed: i32,
-    direction: Direction,
-    current_frame: i32,
+pub enum MovementCommand {
+    Stop,
+    Move(Direction),
 }
 
 /// Returns the row of the spritesheet corresponding to the given direction
@@ -37,102 +33,93 @@ fn direction_spritesheet_row(direction: Direction) -> i32 {
     }
 }
 
-fn render(canvas: &mut WindowCanvas, color: Color, texture: &Texture, player: &Player) -> Result<(), String> {
-    canvas.set_draw_color(color);
-    canvas.clear();
-    let (width, height) = canvas.output_size()?;
+/// Create animation frames for the standard character spritesheet
+fn character_animation_frames(spritesheet: usize, top_left_frame: Rect, direction: Direction) -> Vec<Sprite> {
+    // All assumptions about the spritesheets are now encapsulated in this function instead of in
+    // the design of our entire system. We can always replace this function, but replacing the
+    // entire system is harder.
 
-    let (frame_width, frame_height) = player.sprite.size();
-    let current_frame = Rect::new(
-        player.sprite.x() + frame_width as i32 * player.current_frame,
-        player.sprite.y() + frame_height as  i32 * direction_spritesheet_row(player.direction),
-        frame_width,
-        frame_height,
-    );
+    let (frame_width, frame_height) = top_left_frame.size();
+    let y_offset = top_left_frame.y() + frame_height as i32 * direction_spritesheet_row(direction);
 
-    // Treat the center of the screen as the (0, 0) coordinate
-    let screen_position = player.position + Point::new(width as i32 / 2, height as i32 / 2);
-    //println!("{:?}", screen_position);
-    //let screen_rect = Rect::from_center(screen_position, player.sprite.width(), player.sprite.height());
-    let screen_rect = Rect::from_center(screen_position, frame_width, frame_height);
-    //println!("{:?}", screen_rect);
-    //canvas.copy(texture, None, None)?;
-    //canvas.copy(texture, player.sprite, screen_rect)?;
-    canvas.copy(texture, current_frame, screen_rect)?;
-    
-    canvas.present();
-    Ok(())
-}
-
-// Update player a fixed amount based on their speed.
-// WARNING: Calling this function too often or at a variable speed will cause the player's speed to be unpredictable!
-fn update_player(player: &mut Player) {
-    use self::Direction::*;
-    match player.direction {
-        Left => {
-            player.position = player.position.offset(-player.speed, 0);
-        },
-        Right => {
-            player.position = player.position.offset(player.speed, 0);
-        },
-        Up => {
-            player.position = player.position.offset(0, -player.speed);
-        },
-        Down => {
-            player.position = player.position.offset(0, player.speed);
-        },
+    let mut frames = Vec::new();
+    for i in 0..3 {
+        frames.push(Sprite {
+            spritesheet,
+            region: Rect::new(
+                top_left_frame.x() + frame_width as i32 * i,
+                y_offset,
+                frame_width,
+                frame_height,
+            ),
+        })
     }
 
-    // Only continue to animate if the player is moving
-    if player.speed != 0 {
-        // Cheat: using the fact that all animations are 3 frames (NOT extensible)
-        player.current_frame = (player.current_frame + 1) % 3;
-    }
+    frames
 }
 
 fn main() -> Result<(), String> {
     let sdl_context = sdl2::init()?;
     let video_subsystem = sdl_context.video()?;
-
     // Leading "_" tells Rust that this is an unused variable that we don't care about. It has to
     // stay unused because if we don't have any variable at all then Rust will treat it as a
     // temporary value and drop it right away!
     let _image_context = image::init(InitFlag::PNG | InitFlag::JPG)?;
 
-    let window = video_subsystem.window("SDL2 Game Tutorial", 800, 600)
+    let window = video_subsystem.window("game tutorial", 800, 600)
         .position_centered()
         .build()
-        .expect("Could not initialize the video subsystem");
+        .expect("could not initialize video subsystem");
 
     let mut canvas = window.into_canvas().build()
-        .expect("Could not make a canvas");
-
+        .expect("could not make a canvas");
     let texture_creator = canvas.texture_creator();
-    let texture = texture_creator.load_texture("assets/bardo.png")?;    
-        
-    /*canvas.set_draw_color(Color::RGB(0, 255, 255));
-    canvas.clear();
-    canvas.present();*/
 
-    /*let position = Point::new(0, 0);
-    // src position in the spritesheet
-    let sprite = Rect::new(0, 0, 26, 36);*/
+    let mut dispatcher = DispatcherBuilder::new()
+        .with(keyboard::Keyboard, "Keyboard", &[])
+        .with(physics::Physics, "Physics", &["Keyboard"])
+        .with(animator::Animator, "Animator", &["Keyboard"])
+        .build();
 
-    let mut player = Player {
-        position: Point::new(0, 0),
-        sprite: Rect::new(0, 0, 26, 36),
-        speed: 0,
-        direction: Direction::Right,
+    let mut world = World::new();
+    //dispatcher.setup(&mut world.res);
+    dispatcher.setup(&mut world);
+    //renderer::SystemData::setup(&mut world.res);
+    renderer::SystemData::setup(&mut world);
+
+    // Initialize resource
+    let movement_command: Option<MovementCommand> = None;
+    //world.add_resource(movement_command);
+    world.insert(movement_command);
+
+    let textures = [
+        texture_creator.load_texture("assets/bardo.png")?,
+    ];
+    // First texture in textures array
+    let player_spritesheet = 0;
+    let player_top_left_frame = Rect::new(0, 0, 26, 36);
+
+    let player_animation = MovementAnimation {
         current_frame: 0,
+        up_frames: character_animation_frames(player_spritesheet, player_top_left_frame, Direction::Up),
+        down_frames: character_animation_frames(player_spritesheet, player_top_left_frame, Direction::Down),
+        left_frames: character_animation_frames(player_spritesheet, player_top_left_frame, Direction::Left),
+        right_frames: character_animation_frames(player_spritesheet, player_top_left_frame, Direction::Right),
     };
+
+    world.create_entity()
+        .with(KeyboardControlled)
+        .with(Position(Point::new(0, 0)))
+        .with(Velocity {speed: 0, direction: Direction::Right})
+        .with(player_animation.right_frames[0].clone())
+        .with(player_animation)
+        .build();
 
     let mut event_pump = sdl_context.event_pump()?;
     let mut i = 0;
-
     'running: loop {
-        /*i = (i + 1) % 255;
-        canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
-        canvas.clear();*/
+        // None - no change, Some(MovementCommand) - perform movement
+        let mut movement_command = None;
         // Handle events
         for event in event_pump.poll_iter() {
             match event {
@@ -141,41 +128,39 @@ fn main() -> Result<(), String> {
                     break 'running;
                 },
                 Event::KeyDown { keycode: Some(Keycode::Left), repeat: false, .. } => {
-                    player.speed = PLAYER_MOVEMENT_SPEED;
-                    player.direction = Direction::Left;
+                    movement_command = Some(MovementCommand::Move(Direction::Left));
                 },
                 Event::KeyDown { keycode: Some(Keycode::Right), repeat: false, .. } => {
-                    player.speed = PLAYER_MOVEMENT_SPEED;
-                    player.direction = Direction::Right;
+                    movement_command = Some(MovementCommand::Move(Direction::Right));
                 },
                 Event::KeyDown { keycode: Some(Keycode::Up), repeat: false, .. } => {
-                    player.speed = PLAYER_MOVEMENT_SPEED;
-                    player.direction = Direction::Up;
+                    movement_command = Some(MovementCommand::Move(Direction::Up));
                 },
                 Event::KeyDown { keycode: Some(Keycode::Down), repeat: false, .. } => {
-                    player.speed = PLAYER_MOVEMENT_SPEED;
-                    player.direction = Direction::Down;
+                    movement_command = Some(MovementCommand::Move(Direction::Down));
                 },
                 Event::KeyUp { keycode: Some(Keycode::Left), repeat: false, .. } |
                 Event::KeyUp { keycode: Some(Keycode::Right), repeat: false, .. } |
                 Event::KeyUp { keycode: Some(Keycode::Up), repeat: false, .. } |
                 Event::KeyUp { keycode: Some(Keycode::Down), repeat: false, .. } => {
-                    player.speed = 0;
+                    movement_command = Some(MovementCommand::Stop);
                 },
                 _ => {}
             }
         }
+
+        *world.write_resource() = movement_command;
+
         // Update
         i = (i + 1) % 255;
-
-        //Update player movement
-        update_player(&mut player);
+        //dispatcher.dispatch(&mut world.res);
+        dispatcher.dispatch(&world);
+        world.maintain();
 
         // Render
-        //render(&mut canvas, Color::RGB(i, 64, 255 - i), &texture, position, sprite)?;
-        render(&mut canvas, Color::RGB(i, 64, 255 - i), &texture, &player)?;
+        renderer::render(&mut canvas, Color::RGB(i, 64, 255 - i), &textures, world.system_data())?;
 
-        //canvas.present();
+        // Time management!
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 20));
     }
 
